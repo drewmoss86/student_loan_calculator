09ebcaf77fb0cd58be876a9c989620bb817cbe56
Andrew <drew.moss86@gmail.com>
2019-04-20 16:32:54 -0700

    Add project

---
+++ StudentLoanCalculatorProject/SplashScreen1.vb
@@ -0,0 +1,14 @@
+﻿Public NotInheritable Class SplashScreen1
+
+    'TODO: This form can easily be set as the splash screen for the application by going to the "Application" tab
+    '  of the Project Designer ("Properties" under the "Project" menu).
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(10)
+        If ProgressBar1.Value = 100 Then
+            Me.Hide()
+            frmMain.Show()
+        End If
+    End Sub
+End Class
